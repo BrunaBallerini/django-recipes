669498ed02a095afa82055f31a651f4007614210
Bruna Ballerini <brunaballerini@gmail.com>
2024-03-25 15:53:09 -0300

    Creating template home.html, starting to using reder from django.shortcuts on the views and add app on settings.py

--- recipes/views.py
+++ recipes/views.py
@@ -1,15 +1,28 @@
 '''Views from my app recipes'''
 
-from django.http import HttpResponse  # type: ignore[import-untyped]
-
-# from django.shortcuts import render
+# from django.http import HttpResponse  # type: ignore[import-untyped]
+from django.shortcuts import render  # type: ignore[import-untyped]
 
 
 def my_view_home(request):
     '''View home'''
-    return HttpResponse('home')
+    context = {
+        'site_title': 'Recipes',
+    }
+    return render(
+        request,
+        'recipes/home.html',
+        context,
+    )
 
 
 def my_view_about(request):
     '''View about'''
-    return HttpResponse('about')
+    context = {
+        'site_title': 'About',
+    }
+    return render(
+        request,
+        'recipes/home.html',
+        context,
+    )
